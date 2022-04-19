724f6f63ce1ca333d8c9b1a2f1cb27550275d770
Hermassi Nadir <69248484+Hernado998@users.noreply.github.com>
2022-04-19 17:03:48 +0200

    Update gpssocketreader.cpp

--- src/gpssocketreader.cpp
+++ src/gpssocketreader.cpp
@@ -24,7 +24,7 @@ bool GpsSocketReader::connect()
 	char *source_server = NULL;
 	char *source_port = NULL;
 
-	if (open("/dev/ttyAMA0")) {
+	if (open("/dev/ttyAMA0",O_RDONLY)) {
 		(void)fprintf(stderr,
 		  "gpspipe: could not connect to gpsd %s:%s, %s(%d)\n",
 		  source_server, source_port, strerror(errno), errno);
