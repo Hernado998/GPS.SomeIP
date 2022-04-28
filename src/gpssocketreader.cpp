44452f58adeee6e7f4110499ad7ece540130a765
Hernado998 <nadir.hermassi@supcom.tn>
2022-04-28 11:52:46 +0200

    mmp

--- src/gpssocketreader.cpp
+++ src/gpssocketreader.cpp
@@ -7,7 +7,59 @@
 #include <fcntl.h> 
 #include <errno.h>
 #include <termios.h>
+#define TERMINAL    "/dev/ttyAMA0"
+int fd;
 
+int set_interface_attribs(int fd, int speed)
+{
+    struct termios tty;
+
+    if (tcgetattr(fd, &tty) < 0) {
+        printf("Error from tcgetattr: %s\n", strerror(errno));
+        return -1;
+    }
+
+    cfsetospeed(&tty, (speed_t)speed);
+    cfsetispeed(&tty, (speed_t)speed);
+
+    tty.c_cflag |= (CLOCAL | CREAD);    /* ignore modem controls */
+    tty.c_cflag &= ~CSIZE;
+    tty.c_cflag |= CS8;         /* 8-bit characters */
+    tty.c_cflag &= ~PARENB;     /* no parity bit */
+    tty.c_cflag &= ~CSTOPB;     /* only need 1 stop bit */
+    tty.c_cflag &= ~CRTSCTS;    /* no hardware flowcontrol */
+
+    /* setup for non-canonical mode */
+    tty.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL | IXON);
+    tty.c_lflag &= ~(ECHO | ECHONL | ICANON | ISIG | IEXTEN);
+    tty.c_oflag &= ~OPOST;
+
+    /* fetch bytes as they become available */
+    tty.c_cc[VMIN] = 1;
+    tty.c_cc[VTIME] = 1;
+
+    if (tcsetattr(fd, TCSANOW, &tty) != 0) {
+        printf("Error from tcsetattr: %s\n", strerror(errno));
+        return -1;
+    }
+    return 0;
+}
+
+void set_mincount(int fd, int mcount)
+{
+    struct termios tty;
+
+    if (tcgetattr(fd, &tty) < 0) {
+        printf("Error tcgetattr: %s\n", strerror(errno));
+        return;
+    }
+
+    tty.c_cc[VMIN] = mcount ? 1 : 0;
+    tty.c_cc[VTIME] = 5;        /* half second timer */
+
+    if (tcsetattr(fd, TCSANOW, &tty) < 0)
+        printf("Error tcsetattr: %s\n", strerror(errno));
+}
 
 GpsSocketReader::GpsSocketReader()
 	: m_listener(nullptr)
@@ -19,61 +71,19 @@ void GpsSocketReader::init()
 {
 	connect();
 }
-int
-set_interface_attribs (int fd, int speed, int parity)
-{
-        struct termios tty;
-        if (tcgetattr (fd, &tty) != 0)
-        {
-                //error_message ("error %d from tcgetattr", errno);
-                return -1;
-        }
 
-        cfsetospeed (&tty, speed);
-        cfsetispeed (&tty, speed);
-
-        tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8;     // 8-bit chars
-        // disable IGNBRK for mismatched speed tests; otherwise receive break
-        // as \000 chars
-        tty.c_iflag &= ~IGNBRK;         // disable break processing
-        tty.c_lflag = 0;                // no signaling chars, no echo,
-                                        // no canonical processing
-        tty.c_oflag = 0;                // no remapping, no delays
-        tty.c_cc[VMIN]  = 0;            // read doesn't block
-        tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
-
-        tty.c_iflag &= ~(IXON | IXOFF | IXANY); // shut off xon/xoff ctrl
-
-        tty.c_cflag |= (CLOCAL | CREAD);// ignore modem controls,
-                                        // enable reading
-        tty.c_cflag &= ~(PARENB | PARODD);      // shut off parity
-        tty.c_cflag |= parity;
-        tty.c_cflag &= ~CSTOPB;
-        tty.c_cflag &= ~CRTSCTS;
-
-        if (tcsetattr (fd, TCSANOW, &tty) != 0)
-        {
-                //error_message ("error %d from tcsetattr", errno);
-                return -1;
-        }
-        return 0;
-}
 bool GpsSocketReader::connect()
 {
-	char *source_server = NULL;
-	char *source_port = NULL;
-	int fd;
-	fd=open("/dev/ttyAMA0",O_RDWR | O_NOCTTY | O_SYNC);
-	if (fd == -1) {
-		(void)fprintf(stderr,
-		  "gpspipe: could not connect to gpsd %s:%s, %s(%d)\n",
-		  source_server, source_port, strerror(errno), errno);
-		return false;
-	}
-	set_interface_attribs (fd, B9600, 0);
-	unsigned int flags = WATCH_ENABLE | WATCH_NMEA;
-	char *source_device = NULL;
-	
+	char *portname = TERMINAL; 
+    int wlen;
+	fd = open(portname, O_RDWR | O_NOCTTY | O_SYNC);
+    if (fd < 0) {
+        printf("Error opening %s: %s\n", portname, strerror(errno));
+        return -1;
+    }
+    /*baudrate 115200, 8 bits, no parity, 1 stop bit */
+    set_interface_attribs(fd, B9600);
+
 	return true;
 }
 
@@ -123,7 +133,7 @@ std::string GpsSocketReader::internalRead()
 	
 	char buf[256];
 	std::string l_ret;
-	r = (int)read(gpsdata.gps_fd, buf, sizeof(buf));
+	r = read(fd, buf, sizeof(buf));
 	int i = 0;
 	for (i = 0; i < r; i++) {
 		char c = buf[i];
